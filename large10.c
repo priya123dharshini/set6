269c398263b464677da85663fcb64bb9e2f9f390
priya123dharshini <30829459+priya123dharshini@users.noreply.github.com>
2018-01-21 16:22:38 +0530

    Create large10.c

---
+++ large10.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<math.h>
+main()
+{
+ int i,greatest,a[10];
+ printf("ENTER 10 NUMBERS");
+ for(i=0;i<10;i++)
+ {
+  scanf("%d",a[i]);
+  }
+  a[0=greatest;
+  for(i=0;i<10;i++)
+  {
+  if(a[i]>greatest)
+   greatest=a[i];
+   }
+   printf("the greatest id %d",greatest);
+   }
