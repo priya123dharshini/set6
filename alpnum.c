58bd53a663ff08593255347cf731603f2c9a4ac0
priya123dharshini <30829459+priya123dharshini@users.noreply.github.com>
2018-02-02 14:38:51 +0530

    Create alpnum.c

---
+++ alpnum.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+void main()
+{
+ char s[10];
+ int sum=0,count=0,i;
+ printf("enter a  string");
+ for(i=0;i<s[i];i++)
+  {
+ scanf("%s",&s[10]);
+  }
+ for(i=0;i<s[i];i++)
+ {
+ if((s[i]<='a'||s[i]>='z')||(s[i]<='1'||s[i]>='9'))
+ {
+    if(s[i]<='a'||s[i]>='z')
+     {
+      printf("the number of alphabets is %d",++count);
+      }
+     if(s[i]<='1'||s[i]>='9')
+       { 
+          printf("the number of numbers %d",++sum);
+         }
+      else
+        {
+          printf("no integers,alphabets");
+            }
+}} }          
+
+
+
+
+ 
